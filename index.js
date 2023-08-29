4e3eb7b3d427b25d35bae43faa77b82946ba7ab7
ryip94 <ryip94@yahoo.com>
2023-08-28 18:32:29 -0700

    New repo

--- index.js
+++ index.js
@@ -1,80 +1,65 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-import {getDatabase, ref, push, onValue, remove} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-const appSettings = {
-    databaseURL: "https://grocerypal-955f0-default-rtdb.firebaseio.com/"
-}
+const addBtn = document.getElementById("add-button");
+const input = document.getElementById("input-field");
+let itemList = document.getElementById("item-list");
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const shoppingListInDB = ref(database, "shoppingList")
+document.addEventListener("DOMContentLoaded", function () {
+  let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
 
-const addBtn = document.getElementById('add-button');
-const input = document.getElementById('input-field');
-let itemList = document.getElementById('item-list');
-
-onValue(shoppingListInDB, function(snapshot) {
-    
-    if (snapshot.exists()) {
-        let data = Object.entries(snapshot.val());
-
-        console.log(snapshot.val())
-
-        clearList()
-
-        for (let i = 0; i < data.length; i++) {
-            let currentItem = data[i]
-            let currentItemID = currentItem[0]
-            let currentItemValue = currentItem[1]
-            
-            addItem(currentItem)
-        }
-    } else {
-        itemList.innerHTML = '';
-    }
- 
-})
+  shoppingList.forEach((item) => {
+    addItem(item);
+  });
+});
 
 function clearList() {
-    itemList.innerHTML = '';
+  itemList.innerHTML = "";
 }
 
 function clearItem() {
-    input.value = '';
+  input.value = "";
 }
 
 function addItem(item) {
-    let itemID = item[0]
-    let itemValue = item[1]
+  let itemValue = item;
 
-   
-    let newEl = document.createElement('li');
-    newEl.textContent = itemValue;
+  let newEl = document.createElement("li");
+  newEl.textContent = itemValue;
 
-    newEl.addEventListener('click', () => {
-        remove(ref(database, `shoppingList/${itemID}`))
-    })
+  newEl.addEventListener("click", () => {
+    deleteItemFromLocalStorage(itemValue);
+    newEl.remove();
+  });
 
-    itemList.appendChild(newEl);
+  itemList.appendChild(newEl);
 }
 
+function saveToLocalStorage(item) {
+  let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+  shoppingList.push(item);
+  localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+}
 
+function deleteItemFromLocalStorage(item) {
+  let shoppingList = JSON.parse(localStorage.getItem("shoppingList")) || [];
+  let index = shoppingList.indexOf(item);
+  if (index > -1) {
+    shoppingList.splice(index, 1);
+  }
+  localStorage.setItem("shoppingList", JSON.stringify(shoppingList));
+}
 
+addBtn.addEventListener("click", () => {
+  let inputValue = input.value;
+  // Disallow empty input
+  if (inputValue === "") {
+    return;
+  }
 
-addBtn.addEventListener('click', () => { 
-    let inputValue = input.value;
-    //disallow empty input
-    if (inputValue === '') {
-        return;
-    }
-    //add item to DOM list 
-    addItem(inputValue);
+  // Add item to DOM list
+  addItem(inputValue);
 
+  // Save item to localStorage
+  saveToLocalStorage(inputValue);
 
-    //push item to database and 
-    push(shoppingListInDB, inputValue);
-
-    
-    //clear input field after click
-    clearItem();
+  // Clear input field after click
+  clearItem();
 });
-
